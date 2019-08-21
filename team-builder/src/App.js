ad74379a0bcbeae3726b06103da0aba59130cac7
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-08-21 08:54:13 -0700

    form added

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [member, setMember] = useState({username: "", password: ""});
+
+  const handleChange = event => {
+    setMember({...member, [event.target.name]: event.target.value});
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(member.membername);
+    console.log(member.password);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log("Member returned from App", member)}
+
+      <form onSubmit={event => handleSubmit(event)}>
+      <label>
+        Team Member:
+        <input
+          type="text"
+          name="membername"
+          onChange={event => handleChange(event)}
+        />
+      </label>
+
+      <label>
+        Password:
+        <input
+          type="text"
+          name="password"
+          onChange={event => handleChange(event)}
+        />
+      </label>
+
+      <button>Submit!</button>
+      </form>
+      
     </div>
   );
 }
+        
 
 export default App;

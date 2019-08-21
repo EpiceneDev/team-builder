7201eadd57de1eb7584df2be75a36fa0b21ee251
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-08-21 12:43:04 -0700

    form up again

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,45 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
+import MemberForm from './components/MemberForm';
+
 
 function App() {
-  const [member, setMember] = useState({username: "", password: ""});
-
-  const handleChange = event => {
-    setMember({...member, [event.target.name]: event.target.value});
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    console.log(member.membername);
-    console.log(member.password);
-  };
-
+ 
   return (
     <div className="App">
-      {console.log("Member returned from App", member)}
-
-      <form onSubmit={event => handleSubmit(event)}>
-      <label>
-        Team Member:
-        <input
-          type="text"
-          name="membername"
-          onChange={event => handleChange(event)}
-        />
-      </label>
-
-      <label>
-        Password:
-        <input
-          type="text"
-          name="password"
-          onChange={event => handleChange(event)}
-        />
-      </label>
-
-      <button>Submit!</button>
-      </form>
-
+      <MemberForm member={{name: "Bob", email: "some@thing.org", role: "cinnamin"}} />
     </div>
   );
 }
